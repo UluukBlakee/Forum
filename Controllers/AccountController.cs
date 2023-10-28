1b141d87c751d7d0a90b980160942e06f39b03fe
Uluk <talantbuluk@gmail.com>
2023-10-28 20:18:58 +0600

    Added Migrations and Account controller, actions Login and Register, also Login and Register View Models

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,117 @@
+﻿using Forum.Models;
+using Forum.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace Forum.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IWebHostEnvironment appEnvironment;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, IWebHostEnvironment appEnvironment)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            this.appEnvironment = appEnvironment;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                long size = model.Avatar.Length;
+                if (size > 0)
+                {
+                    string filePath = Path.Combine(appEnvironment.WebRootPath, "images", model.Avatar.FileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await model.Avatar.CopyToAsync(stream);
+                    }
+                }
+                string imagePath = $"/images/{model.Avatar.FileName}";
+                var existingUser = await _userManager.FindByEmailAsync(model.Email);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("Email", "Пользователь с такой почтой уже зарегистрирован.");
+                    return View(model);
+                }
+                var existingUserName = await _userManager.FindByNameAsync(model.UserName);
+                if (existingUserName != null)
+                {
+                    ModelState.AddModelError("UserName", "Пользователь с таким никнеймом уже зарегистрирован.");
+                    return View(model);
+                }
+                User user = new User
+                {
+                    Email = model.Email,
+                    UserName = model.UserName,
+                    Avatar = imagePath
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Topics");
+                }
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByEmailAsync(model.Login);
+                if (user == null)
+                {
+                    user = await _userManager.FindByNameAsync(model.Login);
+                }
+                if (user != null)
+                {
+                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(
+                    user,
+                    model.Password,
+                    model.RememberMe,
+                    false
+                    );
+                    if (result.Succeeded)
+                    {
+                        if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                        {
+                            return Redirect(model.ReturnUrl);
+                        }
+
+                        return RedirectToAction("Index", "Topics");
+                    }
+                }
+                ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Topics");
+        }
+    }
+}
---
+++ ViewModels/RegisterViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Forum.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Поле 'Логин' обязательно для заполнения")]
+        [Display(Name = "Логин")]
+        public string? UserName { get; set; }
+        [Required(ErrorMessage = "Поле 'Email' обязательно для заполнения")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Аватар")]
+        public IFormFile? Avatar { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Пароль' обязательно для заполнения")]
+        [StringLength(100, ErrorMessage = "Пароль должен содержать минимум {2} символа.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,}$",
+        ErrorMessage = "Пароль должен содержать как минимум одну заглавную букву, одну строчную букву и одну цифру.")]
+        public string? Password { get; set; }
+        [Required(ErrorMessage = "Поле 'Подтвердить пароль' обязательно для заполнения")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string? PasswordConfirm { get; set; }
+    }
+}
